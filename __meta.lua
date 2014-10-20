df5ca935ca99321696b778cf5da0af8699525f54
Stefano Peluchetti <phd.st.p@gmail.com>
2014-10-20 12:46:32 +0200

    Added __meta.lua

---
+++ __meta.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------------------------------------
+-- Metadata for Xsys.
+-- 
+-- Copyright (C) 2011-2014 Stefano Peluchetti. All rights reserved.
+--
+-- Features, documentation and more: http://www.scilua.org .
+-- 
+-- This file is part of the Xsys library, which is released under the MIT 
+-- license: full text in file LICENSE.TXT in the library's root folder.
+--------------------------------------------------------------------------------
+
+return {
+  version = "1.0.0",
+  require = {
+  }
+}

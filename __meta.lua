0cd7097a1c5b779447c6a73d0f1a0f060273d9c2
Stefano Peluchetti <phd.st.p@gmail.com>
2015-10-06 17:52:40 +0100

    Prepare for v1.0.2.

--- __meta.lua
+++ __meta.lua
@@ -1,9 +1,10 @@
 return {
-  version = "1.0.1",
+  name = "xsys",
+  version = "1.0.2",
   require = {
     luajit = "2.0"
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
   homepage = "http://scilua.org/xsys.html",
-  description = "XSYS - Extended Lua System",
+  description = "extended Lua system",
 }
